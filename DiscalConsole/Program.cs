88de0fd646686ac535d2b4725dacc6dec711d91b
Alberto Melendez <alberto.melendez@afas.nl>
2018-04-14 08:59:45 +0200

    Created console app
    Created DiscalConsole as a VisualStudio console application using the .NetFramework.

---
+++ DiscalConsole/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DiscalConsole
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Discal Console App");
+      Console.ReadKey();
+    }
+  }
+}
