89a9472ff4d6713ba2fc85571378d47b0104d820
Alberto Melendez <alberto.melendez@afas.nl>
2018-04-15 11:12:34 +0200

    Added InputTest project to solution

---
+++ src/Discal.Input.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Discal.Input.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Discal.Input.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("08c5f2b4-0caf-457c-8e99-8090383d4814")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ src/Discal.Input.Tests/Reader/CsvReader.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Discal.Input.Tests
+{
+  [TestClass]
+  public class CsvReader
+  {
+    [TestMethod]
+    public void ReadFromCsvFile_ExpectsFoundationModelInstances()
+    {
+    }
+  }
+}
