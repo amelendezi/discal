575aea2251ff571b0ed5c9fd408602b0092ec58f
Alberto Melendez <alberto.melendez@afas.nl>
2018-04-24 20:07:21 +0200

    Basic read test added.

---
+++ src/Discal.Input.Tests/Reader/CsvReaderTests.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Discal.Input.DomainModel;
+using Discal.Input.Reader;
+using NUnit.Framework;
+
+namespace Discal.Input.Tests
+{
+  [TestFixture]
+  public class CsvReaderTests
+  {
+    public static string InputTestFile = @"C:\amedev\projects\discal\src\Resources\test\input_test.csv";
+
+    [Test]
+    public void ReadFromCsvFile_ExpectsFoundationModelInstances_WithCorrectValues()
+    {
+      IReader reader = new CsvReader();
+      IEnumerable<Foundation> foundations = reader.Read(InputTestFile);
+
+      Assert.AreEqual(expected: 5, actual: foundations.Count());
+      Assert.AreEqual(expected: true, actual: foundations.Any(f => f.Number == 1 && f.Nit == "827000274" && f.Coordinates == "4.670127,-74.110777"));
+      Assert.AreEqual(expected: true, actual: foundations.Any(f => f.Number == 2 && f.Nit == "800114682" && f.Coordinates == "4.672477,-74.070736"));
+      Assert.AreEqual(expected: true, actual: foundations.Any(f => f.Number == 3 && f.Nit == "900376067" && f.Coordinates == "4.709163,-74.055679"));
+      Assert.AreEqual(expected: true, actual: foundations.Any(f => f.Number == 4 && f.Nit == "900423978" && f.Coordinates == "4.579383,-74.153807"));
+      Assert.AreEqual(expected: true, actual: foundations.Any(f => f.Number == 5 && f.Nit == "900226180" && f.Coordinates == "4.686935,-74.150768"));
+    }
+  }
+}
--- src/DiscalInput/Reader/CsvReader.cs
+++ src/DiscalInput/Reader/CsvReader.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Discal.Input.DomainModel;
 
 namespace Discal.Input.Reader
@@ -7,7 +8,29 @@ namespace Discal.Input.Reader
   {
     public IEnumerable<Foundation> Read(string filePath)
     {
-      throw new System.NotImplementedException();
+      string line;
+      System.IO.StreamReader file = new System.IO.StreamReader(filePath);
+      while((line = file.ReadLine()) != null)
+      {
+        yield return Parse(line);
+      }
     }
+
+    private Foundation Parse(string line)
+    {
+      string[] segments = line.Split(';');
+      var foundation = new Foundation()
+      {
+        Number = Int32.Parse(segments[0]),
+        Nit = segments[1],
+        Description = segments[2],
+        Neighborhood = segments[3],
+        Location = segments[4],
+        Address = segments[5],
+        Coordinates = segments[6]
+      };
+      return foundation;
+    }
+
   }
 }
