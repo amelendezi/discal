d4a78b01d7eb052ea942abb3641cdd53096e165d
Alberto Melendez <alberto.melendez@afas.nl>
2018-04-29 12:34:27 +0200

    Added more exception cases with tests to the input reader.

--- src/Discal.Input.Tests/Reader/CsvReaderTests.cs
+++ src/Discal.Input.Tests/Reader/CsvReaderTests.cs
@@ -9,13 +9,15 @@ namespace Discal.Input.Tests
   [TestFixture]
   public class CsvReaderTests
   {
-    public static string InputTestFile = @"C:\amedev\projects\discal\src\Resources\test\input_test.csv";
+    public static string CorrectTestFile = @"C:\amedev\projects\discal\src\Resources\test\input_test_correct.csv";
+    public static string IncorrectTestFile = @"C:\amedev\projects\discal\src\Resources\test\input_test_incorrect.csv";
+    public static string IncompleteTestFile = @"C:\amedev\projects\discal\src\Resources\test\input_test_incomplete.csv";
 
     [Test]
-    public void ReadFromCsvFile_ExpectsFoundationModelInstances_WithCorrectValues()
+    public void ReadFromCsvFile_WithCorrectData_ExpectsFoundationModelInstances_WithCorrectValues()
     {
       IReader reader = new CsvReader();
-      IEnumerable<Foundation> foundations = reader.Read(InputTestFile);
+      IEnumerable<Foundation> foundations = reader.Read(CorrectTestFile);
 
       Assert.AreEqual(expected: 5, actual: foundations.Count());
       Assert.AreEqual(expected: true, actual: foundations.Any(f => f.Number == 1 && f.Nit == "827000274" && f.Coordinates == "4.670127,-74.110777"));
@@ -23,6 +25,27 @@ namespace Discal.Input.Tests
       Assert.AreEqual(expected: true, actual: foundations.Any(f => f.Number == 3 && f.Nit == "900376067" && f.Coordinates == "4.709163,-74.055679"));
       Assert.AreEqual(expected: true, actual: foundations.Any(f => f.Number == 4 && f.Nit == "900423978" && f.Coordinates == "4.579383,-74.153807"));
       Assert.AreEqual(expected: true, actual: foundations.Any(f => f.Number == 5 && f.Nit == "900226180" && f.Coordinates == "4.686935,-74.150768"));
+    }
+
+    [Test]
+    public void ReadFromCsvFile_WithIncompletButCorrectData_ExpectsFoundationModelInstances_WithMinimumRequiredValues()
+    {
+      IReader reader = new CsvReader();
+      IEnumerable<Foundation> foundations = reader.Read(IncompleteTestFile);
+
+      Assert.AreEqual(expected: 3, actual: foundations.Count());
+      Assert.AreEqual(expected: true, actual: foundations.Any(f => f.Number == 1 && f.Nit == "827000274" && f.Coordinates == "4.670127,-74.110777"));
+      Assert.AreEqual(expected: true, actual: foundations.Any(f => f.Number == 2 && f.Nit == "800114682" && f.Coordinates == "4.672477,-74.070736"));
+      Assert.AreEqual(expected: true, actual: foundations.Any(f => f.Number == 3 && f.Nit == "900376067" && f.Coordinates == "4.709163,-74.055679"));
+    }
+
+    [Test]
+    public void ReadFromCsvFile_WithIncorrectData_ExpectsFoundationsToNotBeCreatedAndLinesToBeIgnored()
+    {
+      IReader reader = new CsvReader();
+      IEnumerable<Foundation> foundations = reader.Read(IncorrectTestFile);
+
+      Assert.AreEqual(expected: 0, actual: foundations.Count());
     }
   }
 }
--- src/DiscalInput/Reader/CsvReader.cs
+++ src/DiscalInput/Reader/CsvReader.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using Discal.Input.DomainModel;
 
 namespace Discal.Input.Reader
@@ -12,24 +13,101 @@ namespace Discal.Input.Reader
       System.IO.StreamReader file = new System.IO.StreamReader(filePath);
       while((line = file.ReadLine()) != null)
       {
-        yield return Parse(line);
+        Foundation foundation = TryParse(line);
+        if(foundation != null)
+        {
+          yield return foundation;
+        }
       }
     }
 
-    private Foundation Parse(string line)
+    private Foundation TryParse(string line)
     {
+      Foundation foundation = null;
       string[] segments = line.Split(';');
-      var foundation = new Foundation()
+
+      if(LineHasValidStructure(segments))
       {
-        Number = Int32.Parse(segments[0]),
-        Nit = segments[1],
-        Description = segments[2],
-        Neighborhood = segments[3],
-        Location = segments[4],
-        Address = segments[5],
-        Coordinates = segments[6]
-      };
+        // Try to parse the Id as an Integer
+        if(!Int32.TryParse(segments[0], out int id))
+        {
+          return null;
+        }
+
+        if(!HasValidCoordinates(segments[6]))
+        {
+          return null;
+        }
+
+        foundation = new Foundation()
+        {
+          Number = id,
+          Nit = segments[1],
+          Description = segments[2],
+          Neighborhood = segments[3],
+          Location = segments[4],
+          Address = segments[5],
+          Coordinates = segments[6]
+        };
+      }
       return foundation;
+    }
+
+    private bool LineHasValidStructure(string[] segments)
+    {
+      // Has minimum required size
+      if(segments.Length != 7)
+      {
+        return false;
+      }
+
+      // Line has values (not necesarily valid) in the required segments
+      if(IsNullOrEmpty(segments[0]) || IsNullOrEmpty(segments[1]) || IsNullOrEmpty(segments[6]))
+      {
+        return false;
+      }
+      return true;
+    }
+
+    private bool IsNullOrEmpty(string text)
+    {
+      return text == null || text == string.Empty;
+    }
+
+    private static bool HasValidCoordinates(string coordinates)
+    {
+      string[] splitted = coordinates.Split(',');
+
+      if(splitted.Length != 2)
+      {
+        return false;
+      }
+
+      string latitudText = splitted[0];
+      string longitudeText = splitted[1];
+
+      decimal latitud;
+      if(!decimal.TryParse(latitudText, NumberStyles.Any, CultureInfo.GetCultureInfo("en-US"), out latitud))
+      {
+        return false;
+      }
+
+      decimal longitud;
+      if(!decimal.TryParse(longitudeText, NumberStyles.Any, CultureInfo.GetCultureInfo("en-US"), out longitud))
+      {
+        return false;
+      }
+
+      if(!(latitud < 5.2m && latitud > 4.4m))
+      {
+        return false;
+      }
+
+      if(!(longitud > -75m && longitud < -74m))
+      {
+        return false;
+      }
+      return true;
     }
 
   }
