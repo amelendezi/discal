0504917053018015331b7f465b53ac3b3b75a557
Alberto Melendez <alberto.melendez@afas.nl>
2018-05-23 23:02:38 +0200

    Added test for RequestBatchBuilder
    It behaves correctly by using a buffer size to build batches of comparable distances.

--- src/Discal.Processing.Tests/Builders/RequestBatchBuilderTests.cs
+++ src/Discal.Processing.Tests/Builders/RequestBatchBuilderTests.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using Discal.Model;
+using Discal.Processing.Builders;
 using NUnit.Framework;
 
 namespace Discal.Processing.Tests.Builders
@@ -10,8 +8,25 @@ namespace Discal.Processing.Tests.Builders
   [TestFixture]
   public class RequestBatchBuilderTests
   {
+    [Test]
+    public void BuildRequestBatches_WithSpecificBufferSize_BuildsCorrectNumberOfBatches()
+    {
+      List<Foundation> foundations = new List<Foundation>();
+      foundations.Add(new Foundation() { Nit = "Nit0", Coordinates = "0,0", Number = 0 });
+      foundations.Add(new Foundation() { Nit = "Nit1", Coordinates = "0,0", Number = 1 });
+      foundations.Add(new Foundation() { Nit = "Nit2", Coordinates = "0,0", Number = 2 });
+      foundations.Add(new Foundation() { Nit = "Nit3", Coordinates = "0,0", Number = 3 });
+      foundations.Add(new Foundation() { Nit = "Nit4", Coordinates = "0,0", Number = 4 });
 
+      var batches = RequestBatchBuilder.BuildRequestBatches(foundations.ToArray(), 2);
 
-
+      Assert.AreEqual(expected: 6, actual: batches.Count);
+      Assert.AreEqual(expected: 2, actual: batches[0].FoundationsToCompareAgainst.Count);
+      Assert.AreEqual(expected: 2, actual: batches[1].FoundationsToCompareAgainst.Count);
+      Assert.AreEqual(expected: 2, actual: batches[2].FoundationsToCompareAgainst.Count);
+      Assert.AreEqual(expected: 1, actual: batches[3].FoundationsToCompareAgainst.Count);
+      Assert.AreEqual(expected: 2, actual: batches[4].FoundationsToCompareAgainst.Count);
+      Assert.AreEqual(expected: 1, actual: batches[5].FoundationsToCompareAgainst.Count);
+    }
   }
 }
