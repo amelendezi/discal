452ae96676a7e46f00310f3cf55b2d4656df71ee
Alberto Melendez <alberto.melendez@afas.nl>
2018-05-23 22:37:29 +0200

    Added foundation for request batch builder.

---
+++ src/Discal.Model/RequestBatch.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Discal.Model
+{
+  public class RequestBatch
+  {
+    public RequestBatch(Foundation foundation)
+    {
+      Foundation = foundation;
+      FoundationsToCompareAgainst = new List<Foundation>();
+    }
+
+    public Foundation Foundation { get; set; }
+
+    public List<Foundation> FoundationsToCompareAgainst { get; set; }
+
+    public string RequestUrl { get; set; }
+  }
+}
---
+++ src/Discal.Processing/Builders/RequestBatchBuilder.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using Discal.Model;
+
+namespace Discal.Processing.Builders
+{
+  public static class RequestBatchBuilder
+  {
+    public const int DefaultBufferSize = 100;
+
+    public static List<RequestBatch> BuildRequestBatches(Foundation[] foundations, int bufferSize = 0)
+    {
+      int buffer = bufferSize == 0 ? DefaultBufferSize : bufferSize;
+      var requestBatches = new List<RequestBatch>();
+
+      for(int i = 0; i < foundations.Length; i++)
+      {
+        int bufferCount = 0;
+        var foundationsToCompareAgainst = new List<Foundation>();
+
+        for(int j = (i + 1); j < foundations.Length; j++)
+        {
+          bufferCount++;
+          foundationsToCompareAgainst.Add(foundations[j]);
+
+          if(bufferCount == buffer || j == foundations.Length - 1)
+          {
+            var requestBatch = new RequestBatch(foundations[i]);
+            requestBatch.FoundationsToCompareAgainst = foundationsToCompareAgainst;
+            requestBatches.Add(requestBatch);
+
+            bufferCount = 0;
+            foundationsToCompareAgainst = new List<Foundation>();
+          }
+        }
+      }
+      return requestBatches;
+    }
+  }
+}
